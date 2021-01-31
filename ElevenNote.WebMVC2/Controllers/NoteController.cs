36fc453b2625e1becb6b6bc611fc114e5432dbc7
traderjeffb <traderjeffb@gmail.com>
2021-01-31 09:20:15 -0500

    changed nav bar and added views

--- ElevenNote.WebMVC2/Controllers/NoteController.cs
+++ ElevenNote.WebMVC2/Controllers/NoteController.cs
@@ -16,5 +16,22 @@ namespace ElevenNote.WebMVC2.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+         //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
     }
 }
