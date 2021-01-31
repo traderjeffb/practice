342d4a3d77a3d093c8b08f960584c1b6645ab38d
traderjeffb <traderjeffb@gmail.com>
2021-01-31 08:26:25 -0500

    added list view

---
+++ ElevenNote.WebMVC2/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using ElevenNote2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.WebMVC2.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
