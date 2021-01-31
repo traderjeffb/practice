b6552eb0b9751942d1dc2af97017de06c28b0c32
traderjeffb <traderjeffb@gmail.com>
2021-01-31 10:24:56 -0500

    added note service

---
+++ ElevenNote2.Models/NoteDetail.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote2.Models
+{
+    class NoteDetail
+    {
+    }
+}
