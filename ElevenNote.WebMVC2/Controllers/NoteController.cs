31f8d8ebeba266dd7fa7e1db780d314cfb36e39e
traderjeffb <traderjeffb@gmail.com>
2021-01-31 09:33:00 -0500

    added to controller

--- ElevenNote.WebMVC2/Controllers/NoteController.cs
+++ ElevenNote.WebMVC2/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote2.Models;
+using ElevenNote2.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +15,9 @@ namespace ElevenNote.WebMVC2.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
             return View(model);
         }
 
@@ -27,11 +31,17 @@ namespace ElevenNote.WebMVC2.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(model);
             }
-            return View(model);
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+
+            service.CreateNote(model);
+
+            return RedirectToAction("Index");
         }
     }
 }
